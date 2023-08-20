74bd3d5aecf0a355112617e6924f57d261ad403c
lpkist <137221454+lpkist@users.noreply.github.com>
2023-08-20 20:56:40 -0300

    Update lista3.jl

--- Listas/Lista3/lista3.jl
+++ Listas/Lista3/lista3.jl
@@ -3,6 +3,8 @@ using WooldridgeDatasets, DataFrames, GLM, Pingouin, StatsPlots
 # educ = β0 + β1motheduc + β2fatheduc + β3abil + β4abil2 + u
 # e interprete os resultados.
 htv = DataFrame(wooldridge("htv"))
-@df dados corrplot([:educ :motheduc :fatheduc :abil ], grid = false)
+display(@df htv corrplot([:educ :motheduc :fatheduc :abil ], grid = false))
 modelo = lm(@formula(educ ~ motheduc + fatheduc + abil + abil^2), htv)
+print(modelo)
+display(qqplot(Normal, residuals(modelo)))
 normality(residuals(modelo), method = "shapiro")
